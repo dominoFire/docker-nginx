4fdbf70fc3aa4c0bfa8d9366c40cd6cddf0d8005
Fernando Aguilar <fer.aguilar.reyes@gmail.com>
2016-11-14 19:23:27 -0600

    Documentado scripts para correr el contenedor

--- container/build-image.sh
+++ container/build-image.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
+# Setteamos esta bandera para interrumpir la ejecución
+# del script en caso de un comando fallido
 set -e 
 
+# Hacemos sourcing del script de la definición de variables
+# para el contenedor
 source container/vars.sh
 
+# Con este comando construimos la imagen Docker y le asignamos
+# el nombre de la imagen
 docker build . -t ${IMAGE_NAME}
--- container/vars.sh
+++ container/vars.sh
@@ -1,1 +1,3 @@
+# Definimos el nombre de la imagen en este archivo
+# para mantener un control de versiones
 IMAGE_NAME=my-ngnix:v2
